e6549add854b63409be7a996a7ff4e0248e1261c
kim <kimrishis@gmail.com>
2022-05-18 16:05:27 -0300

    dia 02

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function inscricao() {
+var nome = document.getElementById('txtnome').value
+var idade = document.getElementById('txtidade').value
+var linguagem = document.getElementById('txtlin').value
+window.alert(`"Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!"`)
+
+
+}
